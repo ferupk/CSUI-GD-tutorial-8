d9060212e6852a63c02338742d38b1d1c00305a7
ferupk <feru.pratama@ui.ac.id>
2025-05-16 10:36:30 +0700

    Better spawner readability

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+class_name Player
 
 @export var speed: int = 400
 @export var GRAVITY: int = 1400
--- scripts/Spawner.gd
+++ scripts/Spawner.gd
@@ -5,14 +5,28 @@ extends Node2D
 @export var spawn_rate: float = 0.3
 
 var current_time = 0
+var player_in_range = false
 
 func _ready():
 	current_time = spawn_rate
 
 func _process(delta):
-	current_time -= delta
-	if current_time <= 0:
-		current_time = spawn_rate
-		
-		var new_enemy = enemy.instantiate()
-		add_child(new_enemy)
+	if !player_in_range:
+		current_time -= delta
+		if current_time <= 0:
+			current_time = spawn_rate
+			
+			var new_enemy = enemy.instantiate()
+			add_child(new_enemy)
+
+
+
+func _on_safe_range_body_entered(body: Node2D) -> void:
+	if body is Player:
+		player_in_range = true
+
+
+func _on_safe_range_body_exited(body: Node2D) -> void:
+	if body is Player:
+		player_in_range = false
+		current_time = spawn_rate / 2
